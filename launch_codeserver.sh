7f66b13ee420918b98c5a28ae4cb5ea047f11b1b
priyakasimbeg <priyakasimbeg@users.noreply.github.com>
2022-10-11 11:57:05 -0700

    Update launch_codeserver.sh

--- launch_codeserver.sh
+++ launch_codeserver.sh
@@ -8,10 +8,10 @@ tmux rename-window -t $session:0 'codeserver'
 tmux send-keys -t $session:$codeserver 'code-server --port 8823' C-m
 
 tmux new-window -t $session:1 -n 'ngrok'
-echo "Password for web interface forwarding: "
 tmux send-keys -t $session:ngrok 'ngrok http 8823' C-m
 
 tmux new-window -t $session:2 -n 'password'
+echo "Password for web interface forwarding: "
 tmux send-keys -t $session:password 'cat ~/.config/code-server/config.yaml' C-m
 
 tmux attach-session -t $session:ngrok
