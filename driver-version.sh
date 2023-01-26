f2096c9f6df534bce0f5b3da400e04312f098add
priyakasimbeg <priyakasimbeg@users.noreply.github.com>
2023-01-26 12:31:02 -0800

    Create driver-version.sh

---
+++ driver-version.sh
@@ -0,0 +1,4 @@
+export DRIVER_VERSION=515.65.01
+export DRIVER_UBUNTU_DEB="nvidia-driver-local-repo-ubuntu1804-515.65.01_1.0-1_amd64.deb"
+export DRIVER_UBUNTU_CUDA_VERSION="11.7.1"
+export DRIVER_UBUNTU_PKG=nvidia-driver-515
