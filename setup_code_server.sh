8f330a9b4a49523f82ac64ff33d037e25ad942b1
priyakasimbeg <priyakasimbeg@users.noreply.github.com>
2024-11-18 21:22:48 -0800

    Update setup_code_server.sh

--- setup_code_server.sh
+++ setup_code_server.sh
@@ -3,7 +3,8 @@ cd
 git clone https://github.com/coder/code-server.git                                
 cd ~/code-server
 ./install.sh
-curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
+sudo tar xvzf ./ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
 echo "Enter your authorization token (https://dashboard.ngrok.com/get-started/your-authtoken): "
 read auth_token
 ngrok config add-authtoken ${auth_token}
